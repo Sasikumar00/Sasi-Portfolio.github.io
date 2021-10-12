e2ca64bb92b5d478113b63b56f084217a4e829fb
M. Sasikumar <msasikumarpeace@gmail.com>
2021-10-12 18:21:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let ham = document.getElementsByClassName("hamburger");
 let hammenu = document.getElementsByClassName("hamb-menu");
 let hamclose = document.getElementsByClassName("hamb-close");
 let overlay = document.getElementsByClassName("overlay");
+let portfolioHover = document.getElementsByClassName("portfolio-hover");
+let portfolioModal = document.getElementsByClassName("portfolio-modal");
+let closeModal = document.getElementsByClassName("close-modal")
 
 ham[0].addEventListener("click", () => {
     hammenu[0].classList.add('change');
@@ -21,4 +24,10 @@ overlay[0].addEventListener('click', () => {
     overlay[0].classList.add('hidden');
     hammenu[0].classList.remove('change');
     overlay[0].classList.remove('change');
+})
+portfolioHover[0].addEventListener('click', () => {
+    portfolioModal[0].classList.remove('hide');
+})
+closeModal[0].addEventListener('click', () => {
+    portfolioModal[0].classList.add('hide');
 })
